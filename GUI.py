4c021df2cd318c634457159ca1dbea5e52a96675
oOo-Solar-oOo <50949696+oOo-Solar-oOo@users.noreply.github.com>
2020-08-12 00:27:06 +0700

    Create About Screen

--- GUI.py
+++ GUI.py
@@ -17,6 +17,8 @@ class GUI:
         self.background = pygame.transform.scale(self.background, (Width-50, Height-400))
         self.playbackground = pygame.image.load('maze.png')
         self.playbackground = pygame.transform.scale(self.playbackground, (Width - 50, Height - 50))
+        self.aboutbackground = pygame.image.load('background.png')
+        self.aboutbackground = pygame.transform.scale(self.aboutbackground, (Width, Height))
         self.state = 'init'
         self.isRunning = True
         self.clock = pygame.time.Clock()
@@ -34,7 +36,21 @@ class GUI:
         pygame.display.update()
     def aboutDraw(self):
         self.screen.fill(BLACK)
-        #self.screen.blit(self.playbackground, (50 // 2, 50 // 2))
+        self.caption = pygame.display.set_caption('About')
+        self.screen.blit(self.aboutbackground, (0, 0))
+        text_surf, text_rect = self.font.render("PROGRAMMERS", TOMATO)
+        self.screen.blit(text_surf, (240,100))
+        text_surf, text_rect = self.font.render("18127017 - Nguyen Hoang Nhan", TOMATO)
+        self.screen.blit(text_surf, (150, 150))
+        text_surf, text_rect = self.font.render("18127259 - Kieu Cong Hau", TOMATO)
+        self.screen.blit(text_surf, (150, 200))
+        text_surf, text_rect = self.font.render("18127267 - Tran Dinh Sang", TOMATO)
+        self.screen.blit(text_surf, (150, 250))
+        text_surf, text_rect = self.font.render("18127268 - Tran Thanh Tam", TOMATO)
+        self.screen.blit(text_surf, (150, 300))
+    def levelDraw(self):
+        self.screen.fill(BLACK)
+        #self.screen.blit(self.levelbackground, (50 // 2, 50 // 2))
         pygame.display.update()
     def playEvent(self):
         for event in pygame.event.get():
@@ -44,14 +60,29 @@ class GUI:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.isRunning = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if 400 <= self.mouse[0] <= 600 and 560 <= self.mouse[1] <= 610:
+                    self.state = 'init'
+        self.mouse = pygame.mouse.get_pos()
+        if 400 <= self.mouse[0] <= 600 and 560 <= self.mouse[1] <= 610:
+            self.drawButton(self.screen, backPos, DarkColor, RED, "Back")
+        else:
+            self.drawButton(self.screen, backPos, LightColor, BLACK, "Back")
+        pygame.display.update()
+    def levelEvent(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.isRunning = False
     def getInitEvent(self):
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if 150 <= self.mouse[0] <= 450 and 320 <= self.mouse[1] <= 395:
                     self.state = 'playing'
                 elif 150 <= self.mouse[0] <= 450 and 400 <= self.mouse[1] <= 450:
-                    self.state = 'about'
+                    self.state = 'level'
                 elif 150 <= self.mouse[0] <= 450 and 480 <= self.mouse[1] <= 530:
+                    self.state = 'about'
+                elif 150 <= self.mouse[0] <= 450 and 560 <= self.mouse[1] <= 610:
                     self.isRunning = False
             elif event.type == pygame.QUIT:
                 self.isRunning = False
@@ -60,13 +91,15 @@ class GUI:
             self.drawButton(self.screen, startPos, DarkColor, RED, "Start")
         else:
             self.drawButton(self.screen, startPos, LightColor, BLACK, "Start")
-
         if 150 <= self.mouse[0] <= 450 and 400 <= self.mouse[1] <= 450:
+            self.drawButton(self.screen, levelPos, DarkColor, RED, "Level")
+        else:
+            self.drawButton(self.screen, levelPos, LightColor, BLACK, "Level")
+        if 150 <= self.mouse[0] <= 450 and 480 <= self.mouse[1] <= 530:
             self.drawButton(self.screen, aboutPos, DarkColor, RED, "About")
         else:
             self.drawButton(self.screen, aboutPos, LightColor, BLACK, "About")
-
-        if 150 <= self.mouse[0] <= 450 and 480 <= self.mouse[1] <= 530:
+        if 150 <= self.mouse[0] <= 450 and 560 <= self.mouse[1] <= 610:
             self.drawButton(self.screen, quitPos, DarkColor, RED, "Exit")
         else:
             self.drawButton(self.screen, quitPos, LightColor, BLACK, "Exit")
@@ -82,6 +115,9 @@ class GUI:
             elif self.state == 'about':
                 self.aboutDraw()
                 self.aboutEvent()
+            elif self.state == 'level':
+                self.levelDraw()
+                self.levelEvent()
             else:
                 self.isRunning = False
         self.clock.tick(60)
--- Specification.py
+++ Specification.py
@@ -12,10 +12,15 @@ DarkColor = (75, 75, 75)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+TOMATO = (255,99,71)
 GREEN = (0, 255, 0)
 
 #Font
 initFont = "8514fix.fon"
+
+#Position
 startPos = pygame.Rect(150, 320, 300, 50)
-aboutPos = pygame.Rect(150, 400, 300, 50)
-quitPos = pygame.Rect(150, 480, 300, 50)
+levelPos = pygame.Rect(150, 400, 300, 50)
+aboutPos = pygame.Rect(150, 480, 300, 50)
+quitPos = pygame.Rect(150, 560, 300, 50)
+backPos = pygame.Rect(400,560,150,50)
