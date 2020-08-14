cee77ee54ac924ed06aba4b77897c6a9c49e562b
Tâm Trần Thanh <18127268@student.hcmus.edu.vn>
2020-08-15 00:44:41 +0700

    lv3 lv4 read

--- Source/Map.py
+++ Source/Map.py
@@ -71,34 +71,34 @@ def read_map_level_2(map_input_path, monster_as_wall: bool):
 
     return graph_map, pacman_pos, food_pos, monster_pos_list
 
-
-def read_map_level_3(map_input_path):
-    pacman_pos, raw_map = input_raw(map_input_path)
-
+def init_cells(raw_map, pacman_pos):
     cells = []
-    food_cell_list = []
-    monster_cell_list = []
 
     for y in range(len(raw_map)):
         row = []
         for x in range(len(raw_map[y])):
             if raw_map[y][x] != 1:
-
                 if raw_map[y][x] == 0:
                     row.append(Cell((x, y), []))
-
                 else:
                     row.append(Cell((x, y), [cState(raw_map[y][x])]))
 
                 if pacman_pos == (x, y):
                     row[x].state.append(cState(4))
                     pacman_cell = row[x]
-
             else:
                 row.append(None)
-
         cells.append(row)
 
+    return cells, pacman_cell
+
+def read_map_level_3(map_input_path):
+    pacman_pos, raw_map = input_raw(map_input_path)
+
+    cells, pacman_cell = init_cells(raw_map, pacman_pos)
+
+    food_cell_list = []
+    monster_cell_list = []
     graph_map = {}
 
     for y in range(len(raw_map)):
@@ -124,3 +124,48 @@ def read_map_level_3(map_input_path):
                     graph_map[cur] = graph_map[cur] + [up]
 
     return cells, graph_map, pacman_cell, food_cell_list, monster_cell_list
+
+
+def read_map_level_4(map_input_path):
+    pacman_pos, raw_map = input_raw(map_input_path)
+
+    cells, pacman_cell = init_cells(raw_map, pacman_pos)
+
+    food_cell_list = []
+    monster_cell_list = []
+    graph_cell = {}
+    graph_map = {}
+
+    for y in range(len(raw_map)):
+        for x in range(len(raw_map[y])):
+            if raw_map[y][x] != 1:
+                c_cur = cells[y][x]
+                cur = (x, y)
+
+                if cState.MONSTER in cur.state:
+                    monster_cell_list.append(c_cur)
+                elif cState.FOOD in cur.state:
+                    food_cell_list.append(c_cur)
+
+                graph_cell[c_cur] = []
+                graph_map[cur] = []
+
+                if x - 1 >= 0 and raw_map[y][x - 1] != 1:
+                    c_left = cells[y][x - 1]
+                    graph_cell[c_left] = graph_cell[c_left] + [c_cur]
+                    graph_cell[c_cur] = graph_cell[c_cur] + [c_left]
+
+                    left = (x - 1, y)
+                    graph_map[left] = graph_map[left] + [cur]
+                    graph_map[cur] = graph_map[cur] + [left]
+
+                if y - 1 >= 0 and raw_map[y - 1][x] != 1:
+                    c_up = cells[y - 1][x]
+                    graph_cell[c_up] = graph_cell[c_up] + [c_cur]
+                    graph_cell[c_cur] = graph_cell[c_cur] + [c_up]
+
+                    up = (x, y - 1)
+                    graph_map[up] = graph_map[up] + [cur]
+                    graph_map[cur] = graph_map[cur] + [up]
+
+    return cells, graph_cell, pacman_cell, graph_map, food_cell_list, monster_cell_list
